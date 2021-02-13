78f4ff51ed8db78d85e401f56ff953b10c2ca33b
jose vera <jose.vera@huawei.com>
2021-02-13 12:20:12 -0500

    init commit

---
+++ app/src/main/java/com/intec/test/SettingActivity.java
@@ -0,0 +1,44 @@
+package com.intec.test;
+
+// TODO: import classes from Analytics Kit
+import com.huawei.hms.analytics.HiAnalytics;
+import com.huawei.hms.analytics.HiAnalyticsInstance;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+    private Button btnSave;
+    private EditText editFavorSport;
+    private String strFavorSport;
+
+// TODO: Define a var for Analytics Instance
+    HiAnalyticsInstance instance;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        // TODO: Genarate Analytics Kit Instance
+        instance = HiAnalytics.getInstance(this);
+
+        btnSave = (Button)findViewById(R.id.save_setting_button);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editFavorSport = (EditText)findViewById(R.id.edit_favoraite_sport);
+                strFavorSport = editFavorSport.getText().toString().trim();
+                // TODO: save favorite sport by user setUserProperty
+                instance.setUserProfile("favor_sport",strFavorSport);
+            }
+        });
+    }
+}
